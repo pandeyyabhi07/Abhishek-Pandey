b12a707f9568cc14f91b2854fa1327c96a647870
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-08-25 16:21:23 +0530

    index main file

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected...");
+    app.listen(process.env.PORT, () =>
+      console.log(`🚀 Server running on http://localhost:${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.error("❌ DB Error:", err));
