40a97329df543ad42095546c76a9bf18bf25b2db
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-08-25 16:16:39 +0530

    multer

---
+++ config/multer.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from './cloudinaryConfig.js';
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'uploads',  
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+     transformation: [{ width: 1000, height: 1000, crop: 'limit' }]
+  }
+});
+
+const upload = multer({ storage });
+
+export default upload;
