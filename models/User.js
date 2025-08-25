55f7cf9ae4d77b5344ee473dc501dc15c1f0ba88
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-08-25 16:18:33 +0530

    user

---
+++ models/User.js
@@ -0,0 +1,72 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import crypto from 'crypto';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      maxlength: [50, 'Name cannot exceed 50 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+     
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters'],
+      select: false, 
+    },
+    profileImage: {
+      type: String,
+      default: null,
+    
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      default: function () {
+        return crypto.randomBytes(20).toString('hex');
+      },
+    },
+    verificationCodeExpires: {
+      type: Date,
+      default: function () {
+        return Date.now() + 24 * 60 * 60 * 1000; 
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.isVerificationCodeExpired = function () {
+  return Date.now() > this.verificationCodeExpires;
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
