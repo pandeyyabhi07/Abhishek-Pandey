e7c708e11924aaff69947bf5d9921b6d1a318b88
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-08-25 16:19:01 +0530

    auth

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { register, verifyEmail, login } from "../controllers/authController.js";
+import upload from "../config/multer.js";
+
+const router = express.Router();
+
+router.post("/register", upload.single("profileImage"), register);
+
+router.get("/verify/:code", verifyEmail);
+
+router.post("/login", login);
+
+export default router;
